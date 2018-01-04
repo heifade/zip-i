3f969ef6ee0941fe69de00982a9d1c3315bbcb7b
heifade <heifade@126.com>
2018-01-04 16:23:24 +0800

    no message

---
+++ project-config.ts
@@ -0,0 +1,10 @@
+import { IProjectConfig } from './src/model/IProjectConfig';
+
+
+let projectConfig: IProjectConfig = {
+  projectType: 'node',
+  command: true,
+  documents: true,
+}
+
+export default projectConfig;
